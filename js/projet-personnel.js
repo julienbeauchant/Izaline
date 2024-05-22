756a30db631a8a94c375c65e4ccf636aacb21043
julienbeauchant <julienbeauchant.pro@yahoo.com>
2024-05-22 18:00:15 +0200

    mise a jour

---
+++ js/projet-personnel.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const carouselInner = document.querySelector('.positionCarouselProjetPersonnel');
+    const items = Array.from(carouselInner.children);
+    const cloneCount = Math.ceil(window.innerWidth / (items[0].offsetWidth + 30)) + 1;
+
+    for (let i = 0; i < cloneCount; i++) {
+        items.forEach(item => {
+            const clone = item.cloneNode(true);
+            carouselInner.appendChild(clone);
+        });
+    }
+});
+
+
+
