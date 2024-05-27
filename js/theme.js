1d9a7995ee518248ac9432d331601375841021f9
julienbeauchant <julienbeauchant.pro@yahoo.com>
2024-05-27 20:10:15 +0200

    couleurs et admin

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,12 @@
+let facebookDark = document.querySelector('#facebook')
+let instagramDark = document.querySelector('#instagram')
+let tiktokDark = document.querySelector('#tiktok')
+let userDark = document.querySelector('#user')
+
 document.getElementById('toggleDarkMode').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
+    facebookDark.src = '../img/logo-n/facebook-n.png'
+    instagramDark.src = '../img/logo-n/instagram-n.png'
+    tiktokDark.src = '../img/logo-n/tiktok-n.png'
+    userDark.src = '../img/logo-n/user-n.png'
 });
